feb7966e14880615dbc9beeec941118541f08145
Mayank Gupta <techfreakworm@gmail.com>
2019-08-16 17:53:35 +0530

    Add home page

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,8 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
-
-from . import views
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]
